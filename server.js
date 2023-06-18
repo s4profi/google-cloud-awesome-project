5d27a0bd1e81d5a8b6c91a3a04aef3777088635c
THHa <47177989+s4profi@users.noreply.github.com>
2023-06-18 13:54:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http")
 const server = http.createServer((req, res) => {
     console.log("New Connection")
-    res.end("Hello Awesome")
+    res.end("Hello Awesome <3")
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log("Listening"))
